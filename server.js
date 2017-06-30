3ce33e3044262dd9863d369f3ec32602db8012ec
liujohnson118 <liujohnson118@yahoo.com>
2017-06-30 03:42:38 +0000

    working on stretch

--- server.js
+++ server.js
@@ -27,15 +27,38 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
+wss.broadcastClientsCount=function broadcastClientsCount(){
+  console.log("Current clients are "+wss.clients.size);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      let newMessage={};
+      newMessage['type']='clientsCount';
+      newMessage['count']=wss.clients.size;
+      newMessage['id']=uuidv1();
+      //console.log("user count : "+newMessage.count);
+      console.log(JSON.stringify(newMessage));
+      client.send(JSON.stringify(newMessage));
+    }
+  });
+}
+
+wss.broadcastClientsCount();
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
-  console.log('Client connected');
-
+  wss.broadcastClientsCount();
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    wss.broadcastClientsCount();
+    console.log('Client disconnected');
+    console.log("There are "+wss.clients.size+" users online");
+  });
   ws.on('message',function incoming(data){
+    console.log("incoming data type is ",typeof data);
+    console.log("incoming data is ",data);
     wss.broadcast(data);
+    wss.broadcastClientsCount();
   });
 });
+
